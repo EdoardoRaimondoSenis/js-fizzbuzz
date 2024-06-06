7c60f635bc20332a4beee9c19b9a4e41def475e1
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-06 16:30:11 +0200

    aggiunti commenti piccole correzioni

--- js/myscript.js
+++ js/myscript.js
@@ -1,37 +1,40 @@
-const ul = document.querySelector("ul.prisultato");
+// do una variabile alla classe prisultato
+const ul = document.querySelector(".prisultato");
 
-
+// il for ricomincia fino ad arrivare a 99 cicli
 for (let i = 1; i <= 100; i++) {
+    
+    // creo l'elemento li nell'HTML e gli do la variabile li
     const li = document.createElement("li");
+    
+    // creo gli li dentro l'ul (.prisultato)
+    ul.appendChild(li);
+
+    // creo tanti li quanti cicli compie il for
     li.append(i);
 
+    // se il numero è 3 o multiplo di 3
     if (i == 3 || i%3==0) {
         li.classList.add("risultatotre");
         li.innerHTML = "Buzz"
-        console.log(li);
     } else {
         li.classList.add("risultato");
-
     }
 
+    // se il numero è 5 o multiplo di 5
     if (i == 5 || i%5==0) {
         li.classList.add("risultatocinque");
         li.innerHTML = "Fizz"
-        console.log(li);
     } else {
         li.classList.add("risultato");
-
     }
 
+    // se il numero è sia multiplo di 5 che multiplo di 3 
     if ((i == 3 || i%3==0) && (i == 5 || i%5==0)) {
         li.classList.add("risultatotrecinque");
         li.innerHTML = "FizzBuzz"
-        console.log(li);
     } else {
         li.classList.add("risultato");
-
     }
-
-    ul.appendChild(li);
     
 }
